02b34cd32f6966670a27e9caee3e784d408e1ab5
yogesh bagade <yogeshbagade25@gmail.com>
2020-06-27 00:19:51 +0530

    HIS-122 Fix Added

--- src/main/java/com/assimilate/springboot/web/security/controllers/Car.java
+++ src/main/java/com/assimilate/springboot/web/security/controllers/Car.java
@@ -2,11 +2,19 @@ package com.assimilate.springboot.web.security.controllers;
 
 public class Car {
 
+
 	
 	
 	
 	public void stop()
 	{
 		System.out.println("Changes For HIS-134");
+		
+	}
+
+	public void start()
+	{
+			System.out.println("Changes in HIS-122");
+
 	}
 }
